91bbaa540e0842e848d9dff1839856ea37f39d68
Michelangelo Ascalon <mikeascalon@yahoo.com>
2023-11-29 18:00:42 -0800

    test 4 and 5 working

--- javascript/code-challenges/challenges-03.test.js
+++ javascript/code-challenges/challenges-03.test.js
@@ -65,7 +65,8 @@ For example, notInFirstArray([1,2,3], [1,2,3,4]) returns [4].
 ------------------------------------------------------------------------------------------------ */
 
 const notInFirstArray = (forbiddenValues, arr) => {
-  // Solution code here...
+  let notInFirst = arr.filter((value)=> !forbiddenValues.includes(value));
+  return notInFirst;
 };
 
 /* ------------------------------------------------------------------------------------------------
