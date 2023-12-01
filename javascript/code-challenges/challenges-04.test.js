a48d47df9764608344b981f21ae573d9a147f8f3
Michelangelo Ascalon <mikeascalon@yahoo.com>
2023-11-30 19:14:10 -0800

    test 2 pass

--- javascript/code-challenges/challenges-04.test.js
+++ javascript/code-challenges/challenges-04.test.js
@@ -80,14 +80,7 @@ Ensure that the original people array and stuff objects are unchanged
 ------------------------------------------------------------------------------------------------ */
 
 const newState = (arr) => {
-  return {
-    people:[...people],
-    stuff: {...stuff,
-      cars:[...stuff.cars,'Ford'],
-      toothbrush:'brand new',
-      toys: stuff.toys + 1,
-    },
-  }
+
 
 };
 
