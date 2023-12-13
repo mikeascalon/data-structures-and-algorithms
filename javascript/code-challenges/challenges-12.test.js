47c6adddf01c75a1505f55c8991ba213e1833ca6
Michelangelo Ascalon <mikeascalon@yahoo.com>
2023-12-12 20:53:21 -0800

    test 4 pass

--- javascript/code-challenges/challenges-12.test.js
+++ javascript/code-challenges/challenges-12.test.js
@@ -82,7 +82,11 @@ For example, [ [0,2,5,4], [2,4,10], [] ] should return [ [1, 32], [1024], [] ].
 ------------------------------------------------------------------------------------------------ */
 
 const divisibleByFiveTwoToThePower = (input) => {
-  // Solution code here...
+  return input.map((subArray) => {
+    return subArray
+      .filter((num) => typeof num === 'number' && num % 5 === 0)
+      .map((num) => Math.pow(2, num));
+  });
 };
 
 /* ------------------------------------------------------------------------------------------------
