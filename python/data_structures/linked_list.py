0c228eae205ac36c8dd1c92aa2c3f859f06fa874
mikeascalon <mikeascalon@yahoo.com>
2024-01-16 23:03:31 -0800

    insert before pass

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -12,6 +12,59 @@ class LinkedList:
         new_node.next = self.head
         self.head = new_node
 
+    
+    def append(self, value):
+        new_node = Node(value)
+        if not self.head:
+            self.head = new_node
+            return
+
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+
+        last_node.next = new_node
+
+    def insert_before(self, target_value, new_value):
+        new_node = Node(new_value)
+        if not self.head:
+            self.head = new_node
+            return
+
+        if self.head.value == target_value:
+            new_node.next = self.head
+            self.head = new_node
+            return
+
+        current_node = self.head
+        while current_node.next:
+            if current_node.next.value == target_value:
+                new_node.next = current_node.next
+                current_node.next = new_node
+                return
+            current_node = current_node.next
+
+        current_node = self.head
+        while current_node.next:
+            if current_node.next.value == target_value:
+                new_node.next = current_node.next
+                current_node.next = new_node
+                return
+            current_node = current_node.next
+
+        # If the loop completes and the target value is not found
+        raise TargetError(f"Target value '{target_value}' not found in the list.")
+
+    def insert_after(self, target_value, new_value):
+        new_node = Node(new_value)
+        current_node = self.head
+        while current_node:
+            if current_node.value == target_value:
+                new_node.next = current_node.next
+                current_node.next = new_node
+                return
+            current_node = current_node.next
+
     def includes(self, value):
         current_node = self.head
         while current_node:
