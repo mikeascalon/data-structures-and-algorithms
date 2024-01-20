db6c45ce82ea814d23483399703d320ac170419c
mikeascalon <mikeascalon@yahoo.com>
2024-01-20 15:27:37 -0800

    stack pass the test

--- python/data_structures/stack.py
+++ python/data_structures/stack.py
@@ -1,4 +1,5 @@
-
+from data_structures.linked_list import Node
+from data_structures.invalid_operation_error import InvalidOperationError
 
 class Stack:
     """
@@ -7,6 +8,14 @@ class Stack:
 
     def __init__(self):
         self.top = None
+
+    def is_empty(self):
+        """
+        Returns True if the stack is empty, False otherwise.
+        """
+
+        return self.top is None
+
 
     def push(self, value):
         """
@@ -27,8 +36,8 @@ class Stack:
         """
         # popping from an empty list
         if self.top is None:
-            # TODO: change this to raise an error
-            return None
+            raise InvalidOperationError("Method not allowed on empty collection")
+        
 
         # get the return value
         pop_value = self.top.value
@@ -37,3 +46,20 @@ class Stack:
         self.top = self.top.next
 
         return pop_value
+    
+    def peek(self):
+        """
+        look for the first node on top
+    
+        """
+        if self.top is None:
+
+            raise InvalidOperationError("Method not allowed on empty collection")
+        
+        # get the return value
+        peek_value = self.top.value
+
+
+        self.top = peek_value
+
+        return peek_value
---
+++ python/docs/stack_and_queue/README.md
@@ -0,0 +1,9 @@
+# Challenge Title
+Using a Linked List as the underlying data storage mechanism, implement both a Stack and a Queue
+
+## Whiteboard Process
+
+
+## Approach & Efficiency
+
+## Solution
