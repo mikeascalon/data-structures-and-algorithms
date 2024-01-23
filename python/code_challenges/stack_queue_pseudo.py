0ea991d67cc98114363ac36875224a54f345ae6f
mikeascalon <mikeascalon@yahoo.com>
2024-01-22 18:15:01 -0800

    pass all the test for pseudo_queue

--- python/code_challenges/stack_queue_pseudo.py
+++ python/code_challenges/stack_queue_pseudo.py
@@ -2,4 +2,24 @@ from data_structures.stack import Stack
 
 
 class PseudoQueue:
-    pass
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+
+    def enqueue(self, value):
+        # To enqueue a value, simply push it onto stack1.
+        self.stack1.append(value)
+
+    def dequeue(self):
+        if not self.stack1 and not self.stack2:
+            # Both stacks are empty, the queue is empty.
+            return None
+
+        if not self.stack2:
+            # If stack2 is empty, transfer elements from stack1 to stack2 to reverse the order.
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+
+        # Pop the element from stack2, which simulates dequeuing from the queue.
+        return self.stack2.pop()
+    
