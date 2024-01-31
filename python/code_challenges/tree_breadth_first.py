a418ad49e576ad1d2a441163af3ade3bf4670dbd
mikeascalon <mikeascalon@yahoo.com>
2024-01-30 21:44:25 -0800

    added tree-breadth-first code challenge

--- python/code_challenges/tree_breadth_first.py
+++ python/code_challenges/tree_breadth_first.py
@@ -1,5 +1,25 @@
 from data_structures.binary_tree import BinaryTree
 
 
-def breadth_first():
-    pass
+def breadth_first(tree):
+    if not tree.root:
+        return []
+    
+
+    queue = []  # Queue for breadth-first traversal
+    values = []  # List to store node values
+    queue.append(tree.root)
+
+    while queue:
+        current = queue.pop(0)  # Dequeue the first node in the queue
+
+        # Add the value of current node to the values list
+        values.append(current.value)
+
+        # Enqueue child nodes of the current node
+        if current.left:
+            queue.append(current.left)
+        if current.right:
+            queue.append(current.right)
+
+    return values
